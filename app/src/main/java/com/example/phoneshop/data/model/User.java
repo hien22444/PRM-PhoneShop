11710b20c373749b9dfdd0a8d9ced295e1e68aa2
MQKeyyy03 <maikhanh280@gmail.com>
2025-11-12 09:49:32 +0700

    sua code

--- app/src/main/java/com/example/phoneshop/data/model/User.java
+++ app/src/main/java/com/example/phoneshop/data/model/User.java
@@ -1,12 +1,4 @@
 package com.example.phoneshop.data.model;
 
-// trong file data/model/User.java
 public class User {
-    // ... các trường khác như id, email, fullName ...
-    private String role; // Ví dụ: "admin" hoặc "user"
-
-    // ... getters and setters ...
-    public String getRole() {
-        return role;
-    }
 }
